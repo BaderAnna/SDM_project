c35cff8a32256982b7252fb37fc4e8eb5d273952
Lina Peters <petersl5@students.uni-marburg.de>
2026-07-14 11:15:40 +0200

    rahmencode tss

--- Scripts/07_evaluation_II.R
+++ Scripts/07_evaluation_II.R
@@ -6,6 +6,8 @@
 library(devtools)
 devtools::install_git("https://gitup.uni-potsdam.de/macroecology/mecofun.git")
 
+https://damariszurell.github.io/EEC-MGC/b4_SDM_eval.html # quasi ein Tutorial
+
 # citation
 paste("Zurell, D. (2024).",
       "mecofun: useful functions for macroecology and species distribution modelling",
@@ -16,15 +18,56 @@ paste("Zurell, D. (2024).",
 library(mecofun)
 
 
-# threshold
-optimal.thresholds(DATA = NULL, threshold = 101, which.model = 1:(ncol(DATA)-2), 
-                   model.names = NULL, na.rm = FALSE, opt.methods = NULL, req.sens, req.spec, 
-                   obs.prev = ObsPrev, smoothing = 1, FPC, FNC)
+# hier alle VS reinladen inkl. predictions
 
-data(Anguilla_train)
-m1 <- glm(Angaus ~ poly(SegSumT,2), data=Anguilla_train, family='binomial')
-preds_cv <- crossvalSDM(m1, kfold=5, traindat=Anguilla_train, colname_species = 'Angaus', colname_pred = 'SegSumT')
-evalSDM(Anguilla_train$Angaus, preds_cv)
+
+
+
+# --- 2c: TSS + Threshold via mecofun::evalSDM() ---
+eval_res <- evalSDM(observation = obs, predictions = pred,
+                    thresh.method = thresh.method)
+
+# wahre Praevalenz mit ausgeben (zur Kontrolle/Interpretation)
+eval_res$true_prevalence <- mean(obs)
+eval_res$niche_breadth_sd <- sd_val
+
+eval_res
+}
+
+
+# -----------------------------------------------------------------------------
+# 3 - Loop ueber alle Nischenbreiten
+# -----------------------------------------------------------------------------
+
+results_list <- lapply(niche_breadths, function(sd_val) {
+  run_one_species(sd_val = sd_val, env = env, n_points = n_points,
+                  thresh.method = "ObsPrev") # hier wird die Prevalance approach verwendet
+
+results_df <- do.call(rbind, results_list)
+rownames(results_df) <- NULL
+
+print(results_df)
+
+
+# -----------------------------------------------------------------------------
+# 4 - Ergebnisse speichern und visualisieren
+# -----------------------------------------------------------------------------
+
+write.csv(results_df, "tss_nischenbreiten_vergleich.csv", row.names = FALSE)
+
+# Einfacher Plot: TSS in Abhaengigkeit von Nischenbreite / Praevalenz
+par(mfrow = c(1, 2))
+
+plot(results_df$niche_breadth_sd, results_df$TSS,
+     type = "b", pch = 19,
+     xlab = "Nischenbreite (sd)", ylab = "TSS",
+     main = "TSS vs. Nischenbreite")
+
+plot(results_df$true_prevalence, results_df$TSS,
+     type = "b", pch = 19,
+     xlab = "Wahre Praevalenz", ylab = "TSS",
+     main = "TSS vs. Praevalenz")
+
 
 
 
